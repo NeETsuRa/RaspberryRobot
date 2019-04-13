19227701364159ec8c8b106acb89a056382c9502
Nejc <dev.neetsu@gmail.com>
2019-04-14 00:27:28 +0200

    Formating PHP sites

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <html>
   <head>
     <title>Raspberry Pi Robot Control</title>
-	<link rel="stylesheet" type="text/css" href="css/MainSite.css">
+	  <link rel="stylesheet" type="text/css" href="css/MainSite.css">
   </head>
   <body>
 	  <h1>Diplomska Naloga: Raspberry pi robot</h1>
 	  <h2> 	Mentor: doc. dr. Andrej Taranenko <br>
 			Kandidat: Nejc Ravnjak
 	  </h2>
-	  <h3>Dobrodošli na spletni strani Diplomske naloge Nejca Ravnjak. Spletna stran služi za kontrolo Robota.
+	  <h3>Dobrodoï¿½li na spletni strani Diplomske naloge Nejca Ravnjak. Spletna stran sluï¿½i za kontrolo Robota.
 	  Natancne podatke kako lahko zgradite in sprogramirate svojega robota lahko preberete v diplomski nalogi.</h3>
 	  
 	  <input type="button" value="Kontrola Robota" onclick="client.html" class="btn btn-primary">
